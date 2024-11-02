ea7883f0609b5105cf184b6d2f930610f2df3c4b
melanAm <42567187+melanAm@users.noreply.github.com>
2024-11-02 15:33:07 +0330

    Update kw_fit.m

--- kw_fit.m
+++ kw_fit.m
@@ -2,11 +2,6 @@ close all;
 clear all; 
 clc;
 
-addpath('E:\Dr. Anooshahpour project\sounds\Kettle Whistle\anechoic\Day10')
-addpath('E:\Dr. Anooshahpour project\sounds\Kettle Whistle\reverb\Day12')
-addpath('E:\Dr. Anooshahpour project\Matlab\utility codes')
-addpath('E:\Dr. Anooshahpour project\Matlab\data')
-
 load('kw_top3fpks_fanposition1.mat');
 load('kw_top3fpks_fanposition2.mat');
 load('kw_top3fpks_fanposition3.mat');
@@ -35,19 +30,13 @@ pos6 = ones(size(fpks_6,1),1)*6;
 pos7 = ones(size(fpks_7,1),1)*7;
 pos8 = ones(size(fpks_8,1),1)*8;
 
-% Load sample data (for example: Fisher's iris dataset)  
-% load fisheriris  
-% X = meas; % Features  
-% Y = species; % Classes  
 
 X = [fpks_1;fpks_2;fpks_3;fpks_4;fpks_5;fpks_6;fpks_7;fpks_8]; % Features  
 Y = [pos1;pos2;pos3;pos4;pos5;pos6;pos7;pos8];; % Classes  
 
 % Create a One-vs-All SVM classifier  
-% Use fitcecoc to create a multi-class SVM model  
 SVMModel = fitcecoc(X, Y); 
 
-% Display the trained model  
 disp(SVMModel);  
 
 % Test the model with a new sample  
@@ -55,7 +44,7 @@ newSample = [1803.50,3573.07,1052.76]; % Example input
 predictedClass = predict(SVMModel, newSample);  
 
 error = resubLoss(SVMModel)
-% Display the predicted class  
+% Display error and the predicted class  
 fprintf('Predicted class: %s\n', predictedClass);
 fprintf('Error: %s\n', error);
  

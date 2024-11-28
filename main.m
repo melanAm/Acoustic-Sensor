ff91ff660717b54751fb9e4b4b4ff0e5a3a6d4e1
melanAm <42567187+melanAm@users.noreply.github.com>
2024-11-28 23:14:30 +0330

    fit degree 1 polynomial to siren data

---
+++ main.m
@@ -0,0 +1,66 @@
+close all;
+clear; 
+clc;
+
+addpath('E:\Acoustic Sensor\Acoustic Sensor\sounds\Siren\anechoic\Day5_d_10cm')
+addpath('E:\Acoustic Sensor\Acoustic Sensor\Matlab\utility')
+addpath('E:\Acoustic Sensor\Acoustic Sensor\Matlab\f0 detection')
+
+% STFT parameters 
+M = 14400;  %300ms
+H = M/2;
+N = 16384;
+win = blackman(M);
+
+fundamental_freqs = zeros(1,6);
+plts = [];
+
+for i=3:8
+   file_name = sprintf('fanPosition%i.wav',i);
+   [x, fs] = audioread(file_name);
+    if size(x,2)>1
+        x = mean(x,2);
+    end
+    %last 3 seconds
+    x = x(end:-1:end-3*fs);
+    [mX,ipfreq,ipmag,f,f0] = f0Detection(x,fs,win,H,N,i);
+    fundamental_freqs(i-2) = f0;
+    figure(1)
+    plt = semilogx(f,mX','DisplayName', sprintf('fan position %i',i))
+    hold on
+    plts(end+1) = plt;
+    figure(1)
+    plot(ipfreq,ipmag,'o','MarkerSize',12)
+    grid on
+    title('Siren Anechoic Spectrum')
+    xlabel('frequency (Hz)')
+    ylabel('magnitude (dB)')
+end
+
+figure(1)
+lgds = {'fan position3','fan position4','fan position5','fan position6','fan position7','fan position8'};
+legend(plts,lgds)
+fanSpeeds_avg = [3.58,3.99,4.68,5.36,5.98,6.64,7.71,8.53];
+
+figure(2)
+plot(fundamental_freqs,fanSpeeds_avg(3:end),'bo')
+hold on
+
+[pred,S] = polyfit(fundamental_freqs,fanSpeeds_avg(3:end),1); 
+
+f0_test = [189,238,295,346,417,473];
+[y_fit,delta] = polyval(pred,fundamental_freqs,S);
+
+plot(fundamental_freqs,y_fit,'r-')
+plot(fundamental_freqs,y_fit+2*delta,'m--',fundamental_freqs,y_fit-2*delta,'m--')
+title('Linear Fit of Siren Data with 95% Prediction Interval')
+xlabel('fundamental frequency (Hz)')
+ylabel('fan speed (m/s)')
+legend('Data','Linear Fit','95% Prediction Interval')
+
+
+
+
+
+
+
